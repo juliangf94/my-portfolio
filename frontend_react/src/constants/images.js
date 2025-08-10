854fb60ea4909f6b4ed57b1a53b2ff574700f372
Julian GONZALEZ <juliangf94@hotmail.com>
2025-08-10 13:30:45 +0200

    Fix: ESLint errors

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -38,7 +38,7 @@ import nb from '../assets/nb.png';
 import skype from '../assets/skype.png';
 import spotify from '../assets/spotify.png';
 
-export default {
+const images = {
   email,
   mobile,
   api,
